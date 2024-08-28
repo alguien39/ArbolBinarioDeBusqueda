201e8ed76489c1e392ec66ea908d47d4c014ca9d
Joham_Barberena <alguien_bama@hotmail.com>
2024-08-28 07:39:58 -0500

    Arbol Binarios De Busqueda

---
+++ ClaseABB.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArbolBinarioDeBusqueda
+{
+    class ClaseABB<Tipo> where Tipo: IEquatable<Tipo>, IComparable<Tipo>
+    {
+        private ClaseNodo<Tipo> raiz;
+        public ClaseNodo<Tipo> Raiz
+        {
+            get { return raiz; }
+            private set { raiz = value; }
+        }
+
+        public ClaseABB()
+        {
+            Raiz = null;
+        }
+
+        public bool Vacio
+        {
+            get
+            {
+                if(Raiz == null) {return true;}
+                return false;
+            }
+        }
+
+        public void AgregarNodo(Tipo objeto)
+        {
+            ClaseNodo<Tipo> NodoNuevo = new ClaseNodo<Tipo>();
+            NodoNuevo.ObjetoConDatos = objeto;
+            if (Vacio)
+            {
+                Raiz = NodoNuevo;
+            }
+            else
+            {
+                AgregarNodo(Raiz, NodoNuevo);
+            }
+        }
+
+        private void AgregarNodo(ClaseNodo<Tipo> nodoActual, ClaseNodo<Tipo> nodoNuevo)
+        {
+            if (nodoNuevo.ObjetoConDatos.CompareTo(nodoActual.ObjetoConDatos) == 1)
+            {
+                if (nodoActual.HijoDer == null)
+                {
+                    nodoActual.HijoDer = nodoNuevo;
+                }
+                else
+                {
+                    AgregarNodo(nodoActual.HijoDer, nodoNuevo);
+                }
+            }
+            else if (nodoNuevo.ObjetoConDatos.CompareTo(nodoActual.ObjetoConDatos) == -1)
+            {
+                if (nodoActual.HijoIzq == null)
+                {
+                    nodoActual.HijoIzq = nodoNuevo;
+                }
+                else
+                {
+                    AgregarNodo(nodoActual.HijoIzq, nodoNuevo);
+                }
+            }
+            else
+            {
+                throw new Exception("No se permiten duplicados");
+            }
+        }
+
+        public void EliminarNodo(Tipo objeto)
+        {
+
+        }
+
+        public void BuscarNodo(Tipo objeto)
+        {
+
+        }
+    }
+}
---
+++ ClaseNodo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArbolBinarioDeBusqueda
+{
+    class ClaseNodo<Tipo>
+    {
+        private Tipo objetoConDatos;
+        public Tipo ObjetoConDatos
+        {
+            get { return objetoConDatos; }
+            set { objetoConDatos = value; }
+        }
+
+        private ClaseNodo<Tipo> hijoIzq;
+        public ClaseNodo<Tipo> HijoIzq
+        {
+            get { return hijoIzq; }
+            set { hijoIzq = value; }
+        }
+        private ClaseNodo<Tipo> hijoDer;
+        public ClaseNodo<Tipo> HijoDer
+        {
+            get { return hijoDer; }
+            set { hijoDer = value; }
+        }
+        ~ClaseNodo()
+        {
+            HijoDer = null;
+            hijoIzq = null;
+            objetoConDatos = default(Tipo);
+        }
+    }
+}
